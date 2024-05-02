457d4e902f14d258e7cb575cb99bbb55b847b138
victor <victorlrpf@gmail.com>
2024-05-02 17:00:12 -0300

    feat: add banner pomotional

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
@@ -12,6 +13,19 @@ const Home = () => {
   <div className="px-5 pt-6">
     <CategoryList />
   </div>
+
+  <div>
+    <Image 
+    src="/Banner-promo01.png" 
+    alt="até 30% de desconto em pizzas" 
+    width={0} 
+    height={0}
+    className="w-full h-auto"
+    sizes="100vw"
+    quality={100}
+    />
+  </div>
+
   </>;
 
 };
