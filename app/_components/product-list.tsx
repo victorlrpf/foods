a8df8d3a6e218012d47ec35ec12e35666b5d16aa
victor <victorlrpf@gmail.com>
2024-05-03 16:28:23 -0300

    refactor: add refactor in product List and pageHome

--- app/_components/product-list.tsx
+++ app/_components/product-list.tsx
@@ -1,24 +1,20 @@
+import { Prisma } from "@prisma/client";
 import { db } from "../_lib/prisma";
 import ProductItem from "./product-item";
 
-const ProductList = async () => {
-    
-    const products = await db.product.findMany({
-        where: {
-            discountPercentage: {
-                gt: 0
-            }
-        },
-        take: 10,
-        include: {
-            restaurant:{
-                select: {
-                    name: true
-                }
-            }
-        }
-    })
-    
+interface ProductListProps {
+    products: Prisma.ProductGetPayload<{
+      include: {
+        restaurant: {
+          select: {
+            name: true;
+          };
+        };
+      };
+    }>[];
+  }
+
+const ProductList = async ({ products }: ProductListProps) => {
     return ( 
         <div className=" flex gap-4 overflow-x-scroll h-60 px-5 [&::-webkit-scrollbar]:hidden">
             {products.map((product) => (
